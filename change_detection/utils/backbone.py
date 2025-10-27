b8dfe32bde762c0355f4c90b8d98f09e4d8f4b9c
dyzy41 <dyzy41@whu.edu.cn>
2025-10-27 08:20:50 +0000

    fix backbone bugs

--- change_detection/utils/backbone.py
+++ change_detection/utils/backbone.py
@@ -24,7 +24,7 @@ def build_backbone(model_name='resnet'):
 
     elif model_name=='hrnet_w18':
         try:
-            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'resnet50.a1_in1k/pytorch_model.bin')), features_only=True)
+            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'hrnet_w18.ms_aug_in1k/pytorch_model.bin')), features_only=True)
         except:
             model = timm.create_model(model_name, pretrained=False, features_only=True)
         FPN_DICT = {'type': 'FPN', 'in_channels': [128, 256, 512, 1024], 'out_channels': 256, 'num_outs': 4}
@@ -32,7 +32,7 @@ def build_backbone(model_name='resnet'):
 
     elif model_name=='hrnet_w32':
         try:
-            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'resnet50.a1_in1k/pytorch_model.bin')), features_only=True)
+            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'hrnet_w32.ms_in1k/pytorch_model.bin')), features_only=True)
         except:
             model = timm.create_model(model_name, pretrained=False, features_only=True)
         FPN_DICT = {'type': 'FPN', 'in_channels': [128, 256, 512, 1024], 'out_channels': 256, 'num_outs': 4}
@@ -40,7 +40,7 @@ def build_backbone(model_name='resnet'):
 
     elif model_name=='convnext_base':
         try:
-            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'resnet50.a1_in1k/pytorch_model.bin')), features_only=True)
+            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'convnext_base.clip_laion2b_augreg_ft_in12k_in1/pytorch_model.bin')), features_only=True)
         except:
             model = timm.create_model(model_name, pretrained=False, features_only=True)
         FPN_DICT = {'type': 'FPN', 'in_channels': [128, 256, 512, 1024], 'out_channels': 256, 'num_outs': 4}
@@ -56,7 +56,7 @@ def build_backbone(model_name='resnet'):
 
     elif model_name=='mambaout_base':
         try:
-            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'resnet50.a1_in1k/pytorch_model.bin')), features_only=True)
+            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'mambaout_base.in1k/pytorch_model.bin')), features_only=True)
         except:
             model = timm.create_model(model_name, pretrained=True, features_only=True)
         FPN_DICT = {'type': 'FPN', 'in_channels': [128, 256, 512, 768], 'out_channels': 256, 'num_outs': 4}
@@ -65,7 +65,7 @@ def build_backbone(model_name='resnet'):
 
     elif model_name=='swinv2_base_window8_256':
         try:
-            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'resnet50.a1_in1k/pytorch_model.bin')), features_only=True)
+            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'swinv2_base_window8_256.ms_in1k/pytorch_model.bin')), features_only=True)
         except:
             model = timm.create_model(model_name, pretrained=True, features_only=True)
         FPN_DICT = {'type': 'FPN', 'in_channels': [128, 256, 512, 1024], 'out_channels': 256, 'num_outs': 4}
@@ -74,7 +74,7 @@ def build_backbone(model_name='resnet'):
 
     elif model_name=='swinv2_small_window8_256':
         try:
-            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'resnet50.a1_in1k/pytorch_model.bin')), features_only=True)
+            model = timm.create_model(model_name, pretrained=True, pretrained_cfg_overlay=dict(file=os.path.join(os.environ.get("PRETRAIN"), 'swinv2_small_window8_256.ms_in1k/pytorch_model.bin')), features_only=True)
         except:
             model = timm.create_model(model_name, pretrained=True, features_only=True)
         FPN_DICT = {'type': 'FPN', 'in_channels': [96, 192, 384, 768], 'out_channels': 256, 'num_outs': 4}
